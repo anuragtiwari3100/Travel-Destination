a59211e3c169198fd9f2f238cba5b6ff5e1f6522
Anurag Tiwari <anuragtiwarigpj2005@gmail.con>
2024-06-02 13:03:19 +0530

    initial Commit

---
+++ src/data.js
@@ -0,0 +1,69 @@
+const data = [
+
+	{
+		id: 1,
+		name: "Darjeeling",
+		info: "Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!",
+		image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+		price: "78,595",
+	},
+	
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+		price: "82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.thecodehelp.in/Goa.jpeg",
+		price: "29,695",
+	},
+	{
+		id: 4,
+		name: "Varanasi",
+		info: "Varanasi is a holy city in northern India, situated on the banks of the sacred river Ganges. The city is a major pilgrimage site for Hindus and is famous for its ancient temples, winding alleyways, and colorful festivals.",
+		image: "https://media.istockphoto.com/id/537988165/photo/varanasi.jpg?s=612x612&w=0&k=20&c=fFpEL17MiQJx5NkkNIVrTsesd2E8b04SCgzjfhUmQ7g=",
+		price: "31,095",
+	},
+	{
+		id: 5,
+		name: "Ayodhya",
+		info: "Ayodhya is a city in northern India, known for its rich historical and religious significance. It is believed to be the birthplace of Lord Rama, a major deity in Hinduism, and is home to the Ram Janmabhoomi temple. Ayodhya also features several other historic and spiritual sites, including the Hanuman Garhi temple and Kanak Bhawan.",
+		image: "https://images1.wionews.com/images/wion/900x1600/2023/7/12/1689171813982_F0zuUiBakAANKd8.jpeg",
+		price: "35,758",
+	},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. The city is home to several impressive palaces and fortresses, as well as unique cultural traditions and handicrafts.",
+		image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+		price: "68,595",
+	},
+	{
+		id: 8,
+		name: "Mussoorie",
+		info: "Mussoorie, a hill station in Uttarakhand, is known for its stunning natural beauty and pleasant climate. Nestled in the Himalayas, it offers breathtaking views, colonial-era architecture, and vibrant markets. It's a popular destination for nature lovers and adventure enthusiasts.",
+		image: "https://images.unsplash.com/photo-1583143874828-de3d288be51a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bXVzc29vcmllJTJDJTIwaW5kaWF8ZW58MHx8MHx8fDA%3D",
+		price: "68,595",
+	},
+	{
+		id: 9,
+		name: "Kashmir",
+		info: "Kashmir, often referred to as `Paradise on Earth`, is a region located in the northern part of the India. It is known for its stunning natural beauty, including snow-capped mountains, lush green valleys, and serene lakes. Kashmir has a rich cultural heritage, with influences from the various civilizations that have ruled the region over the centuries.",
+		image: "https://go2kashmir.com/kashmir/img/image/srinagar-city.jpg",
+		price: "68,595",
+	},
+];
+
+export default data;
